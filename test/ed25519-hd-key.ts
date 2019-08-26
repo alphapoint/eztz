e867c41bc1b70bbd918b567cdae787cb9b49de6e
Shaunak Pandit <shaunak.pandit@alphapoint.com>
2019-08-26 14:15:40 -0400

    still failed because it was trying to look through the whole keypair

--- test/ed25519-hd-key.ts
+++ test/ed25519-hd-key.ts
@@ -85,7 +85,7 @@
             path: vector.path,
             key: key.toString('hex'),
             chainCode: chainCode.toString('hex'),
-            publicKey: (await getKeyPair(key, true)).toString('hex')
+            publicKey: (await getKeyPair(key, true)).publicKey.toString('hex')
           }).toEqual(vector);
         });
       });
@@ -104,7 +104,7 @@
             path: vector.path,
             key: key.toString('hex'),
             chainCode: chainCode.toString('hex'),
-            publicKey: (await getKeyPair(key, true)).toString('hex')
+            publicKey: (await getKeyPair(key, true)).publicKey.toString('hex')
           }).toEqual(vector);
         });
       });
